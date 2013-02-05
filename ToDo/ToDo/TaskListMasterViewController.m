8f819b1ed13e166960d4695eff75a201f81b2369
BENJAMIN BUCKMASTER <bpbuckmaster@SH308-18.local>
2013-02-04 23:18:34 -0600

    Added proper data-based adjustment of display and added file saving.

--- ToDo/ToDo/TaskListMasterViewController.m
+++ ToDo/ToDo/TaskListMasterViewController.m
@@ -11,10 +11,9 @@
 
 @interface TaskListMasterViewController () {
     NSMutableArray *_objects;
+    NSMutableArray *_flags; //True for finished tasks. 
 }
 @end
-
-
 
 
 @implementation TaskListMasterViewController
@@ -48,9 +47,25 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
 
-    //NSDate *object = _objects[indexPath.row];
     NSString *object = _objects[indexPath.row];
     cell.textLabel.text = [object description];
+    
+    if(_flags[indexPath.row] == [NSNumber numberWithBool:YES]) //If a task is finished.
+    {
+        cell.textLabel.textColor = [UIColor redColor];
+        NSDictionary* attributes = @{NSStrikethroughStyleAttributeName: [NSNumber numberWithInt:NSUnderlineStyleSingle]};
+        NSAttributedString* attrText = [[NSAttributedString alloc] initWithString:cell.textLabel.text attributes:attributes];
+        cell.textLabel.attributedText = attrText;
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else                    //The task is unfinished.
+    {
+        cell.textLabel.textColor = [UIColor blackColor];
+        cell.textLabel.text =[[NSString alloc]initWithString:cell.textLabel.text];
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    //NSString *object = _tasks[indexPath.row][0];
+
     return cell;
 }
 
@@ -64,6 +79,7 @@
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         [_objects removeObjectAtIndex:indexPath.row];
+        [_flags removeObjectAtIndex:indexPath.row];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
@@ -72,38 +88,44 @@
                                                                     
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //NSLog(@"Selected Cell");
-    
-            //When we select a cell, we if its text is colored red, we return it to normal. 
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     
-    if(cell.textLabel.textColor == [UIColor redColor])
+    //If the task was marked completed, mark it is incomplete and return it to natural text.  
+    if(_flags[indexPath.row] == [NSNumber numberWithBool:YES])
     {
         cell.textLabel.textColor = [UIColor blackColor];
         cell.textLabel.text =[[NSString alloc]initWithString:cell.textLabel.text];
         cell.accessoryType = UITableViewCellAccessoryNone;
+        _flags[indexPath.row] = [NSNumber numberWithBool:NO];
     }
-    else    // Or, if its text is not red, we color it red and strikethrough it. We also turn on the checkmark.
+    else    // Or, if the task is now incomplete, we color it red and strikethrough it.
+            // We also turn on the checkmark.
     {
         cell.textLabel.textColor = [UIColor redColor];
         NSDictionary* attributes = @{NSStrikethroughStyleAttributeName: [NSNumber numberWithInt:NSUnderlineStyleSingle]};
         NSAttributedString* attrText = [[NSAttributedString alloc] initWithString:cell.textLabel.text attributes:attributes];
         cell.textLabel.attributedText = attrText;
         cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        _flags[indexPath.row] = [NSNumber numberWithBool:YES];
     }
   
 }
-
+//This should only be called for User-Generated New tasks. 
 - (void)insertNewObject:(NSString*) str
 {
     if (!_objects){
         _objects = [[NSMutableArray alloc] init];
     }
     
+    if (!_flags){
+        _flags = [[NSMutableArray alloc] init];
+    }
+    
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:_objects.count inSection:0];
-       [_objects insertObject:[NSString stringWithString:(str)] atIndex:_objects.count];
+    [_objects insertObject:[NSString stringWithString:(str)] atIndex:_objects.count];
+    [_flags addObject:[NSNumber numberWithBool:NO]];
     [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-        
+    
 }
 
 
@@ -112,11 +134,36 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *fullpath1 = [documentsDirectory stringByAppendingPathComponent:@"data.plist"];
+    NSString *fullpath2 = [documentsDirectory stringByAppendingPathComponent:@"flags.plist"];
+    NSLog(@"Reading Data");
+    NSMutableArray *arrayFromDisk1  = [NSKeyedUnarchiver unarchiveObjectWithFile:fullpath1];
+    NSMutableArray *arrayFromDisk2 = [NSKeyedUnarchiver unarchiveObjectWithFile:fullpath2];
+    
+    
+    _objects = [[NSMutableArray alloc] initWithArray:arrayFromDisk1];
+    _flags = [[NSMutableArray alloc] initWithArray:arrayFromDisk2];
+    
+    if(_objects.count > 0)
+    {
+        int a = 0;
+        for(;a<_objects.count; a++)
+        {
+            NSIndexPath *indexPath = [NSIndexPath indexPathForRow:a inSection:0];
+            [self.tableView cellForRowAtIndexPath:indexPath];
+        }
+        
+    }
+    
+    
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
     
     UIApplication *app = [UIApplication sharedApplication];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground:)name:UIApplicationDidEnterBackgroundNotification object:app];
-
+    
+    
     
 }
 
@@ -147,10 +194,19 @@
     
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *fullpath = [documentsDirectory stringByAppendingPathComponent:@"data.plist"];
+    NSString *fullpath1 = [documentsDirectory stringByAppendingPathComponent:@"data.plist"];
+    NSString *fullpath2 = [documentsDirectory stringByAppendingPathComponent:@"flags.plist"];
+    
+    NSMutableArray *dataToFile = [[NSMutableArray alloc] initWithArray:_objects];
+    NSMutableArray *flagsToFile = [[NSMutableArray alloc] initWithArray:_flags];
+
+    
+    NSLog(@"Writing Data");
+    [NSKeyedArchiver archiveRootObject:dataToFile toFile:fullpath1];
+    [NSKeyedArchiver archiveRootObject:flagsToFile toFile:fullpath2];
     
 }
-         
+
          
 /*
 // Override to support rearranging the table view.
